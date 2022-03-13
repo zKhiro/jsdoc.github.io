b181e26d52a0019937e028a11c6cdb66396a078c
Leandro <leandro.khiro@gmail.com>
2022-03-13 14:28:27 -0300

    add vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+  @import './scss/styles';
+</style>
