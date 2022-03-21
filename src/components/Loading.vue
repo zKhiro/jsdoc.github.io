6c0e0fcd483d5e5957943ebbf8e3392779839ea3
Leandro <leandro.khiro@gmail.com>
2022-03-20 21:08:27 -0300

    Create loading handler component

---
+++ src/components/Loading.vue
@@ -0,0 +1,40 @@
+<script>
+  import { MoreHorizontalIcon } from "@scarlab/icons-vue/outline";
+
+  export default {
+    props: {
+      loading: false,
+    },
+    components: { MoreHorizontalIcon }
+  }
+</script>
+
+<template>
+  <div :aria-busy="loading" aria-describedby="loading">
+    <slot v-if="!loading" />
+  </div>
+
+  <div v-if="loading" id="loading" aria-label="Content loading...">
+    /** @loading */
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use "@sass/abstract/mixins/animations" as ma;
+  @use "@sass/vendor/bootstrap" as b;
+
+  #loading {
+    animation-direction:        alternate;
+    animation-iteration-count:  infinite;
+    text-align:                 center;
+
+    @include ma.animation(loading, 1.25s) {
+      from {
+        color: b.$dark;
+      }
+      to {
+        color: transparentize(b.$dark, .9);
+      }
+    }
+  }
+</style>
