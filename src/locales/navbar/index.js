80442417efc71983bf5ba6f5976481e18a1849e8
Leandro <leandro.khiro@gmail.com>
2022-03-20 21:24:03 -0300

    Add i18n for navbar

---
+++ src/locales/navbar/index.js
@@ -0,0 +1,26 @@
+export const NavbarI18n = {
+  'en-US': {
+    links: [
+      {
+        "getting-started": {
+          label: "Getting Started",
+          subLinks: {
+            "what-is-jsdoc": "What is JSDoc?",
+            "documenting-your-code": "Documenting your code?",
+            "generate-a-website": "Generate a website",
+          }
+        },
+        "teste": {
+          label: "Teste",
+          subLinks: {
+            "sub-teste-1": "Teste 1",
+            "sub-teste-2": "Teste 2"
+          }
+        }
+      },
+      {
+        title: "Essentials",
+      }
+    ],
+  },
+};
